b06e0821eda11c2f746d7c2a32d1c5c34c5839ba
Duy Ha <ha.le@dinovative.com>
2020-02-12 15:07:38 +0700

    test reset soft

--- demo.js
+++ demo.js
@@ -1,20 +1,14 @@
 function demo() {
   this.name = "abc";
-<<<<<<< HEAD
   this.hasBeautyful = false;
-=======
->>>>>>> b55a38ef5d95f231624272111b112bc7b83bc0b1
 }
 
 demo.prototype.fly = function (x) {
   this.name = x;
 };
 
-<<<<<<< HEAD
 demo.prototype.hasBeautiful = function () {
   this.hasBeautiful = true;
 };
 
-=======
->>>>>>> b55a38ef5d95f231624272111b112bc7b83bc0b1
 modules.exports = demo;
--- index.js
+++ index.js
@@ -3,10 +3,7 @@ function hello() {
   var name = "abc";
   this.bravo = false;
   this.step = 0;
-<<<<<<< HEAD
   this.music = '';
-=======
->>>>>>> b55a38ef5d95f231624272111b112bc7b83bc0b1
 }
 hello.prototype.bravo = function () {
   this.bravo = true;
@@ -15,11 +12,8 @@ hello.prototype.bravo = function () {
 hello.prototype.run = function () {
   this.step += 1;
 };
-<<<<<<< HEAD
 hello.prototype.sing = function (x) {
   this.music = x;
 };
-=======
->>>>>>> b55a38ef5d95f231624272111b112bc7b83bc0b1
 
 modules.exports = hello; 
